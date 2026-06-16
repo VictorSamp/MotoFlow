395b544da288258568d8048f763d4cb87d31c8f2
Victor Sampaio <victormag.sampaio@outlook.com.br>
2026-06-16 11:10:03 -0300

    feat: add member entity and validation tests

---
+++ MotoFlow.Domain/Entities/Member.cs
@@ -0,0 +1,37 @@
+﻿namespace MotoFlow.Domain.Entities
+{
+    public class Member
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public DateTime JoinDate { get; private set; }
+
+        //public PatchLevel CurrentPatchLevel { get; private set; }
+
+        //public MemberStatus Status { get; private set; }
+
+        public Member(string name, string email, string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be null or empty.");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be null or empty.");
+
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                throw new ArgumentException("Phone number cannot be null or empty.");
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            JoinDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ MotoFlow.Tests/Domain/Entities/MemberTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using MotoFlow.Domain.Entities;
+
+namespace MotoFlow.Tests.Domain.Entities
+{
+    public class MemberTests
+    {
+        [Fact]
+        public void Should_Create_Member_When_Data_Is_Valid()
+        {
+            var name = "Victor Sampaio";
+            var email = "victor@email.com";
+            var phoneNumber = "31999999999";
+
+            var member = new Member(name, email, phoneNumber);
+
+            member.Should().NotBeNull();
+            member.Id.Should().NotBeEmpty();
+            member.Name.Should().Be(name);
+            member.Email.Should().Be(email);
+            member.PhoneNumber.Should().Be(phoneNumber);
+            member.JoinDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+            //member.Status.Should().Be(MemberStatus.Prospect);
+            //member.CurrentPatchLevel.Should().Be(PatchLevel.Prospect);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Should_Throw_Exception_When_Name_Is_Invalid(string? name)
+        {
+            var email = "victor@email.com";
+            var phoneNumber = "31999999999";
+
+            Action action = () => new Member(name!, email, phoneNumber);
+
+            action.Should().Throw<ArgumentException>()
+                 .WithMessage("Name cannot be null or empty.");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Should_Throw_Exception_When_Email_Is_Invalid(string? email)
+        {
+            var name = "Victor Sampaio";
+            var phoneNumber = "31999999999";
+
+            Action action = () => new Member(name, email!, phoneNumber);
+
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Email cannot be null or empty.");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Should_Throw_Exception_When_Phone_Number_Is_Invalid(string? phoneNumber)
+        {
+            var name = "Victor Sampaio";
+            var email = "victor@email.com";
+
+            Action action = () => new Member(name, email, phoneNumber!);
+
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("Phone number cannot be null or empty.");
+        }
+    }
+}
